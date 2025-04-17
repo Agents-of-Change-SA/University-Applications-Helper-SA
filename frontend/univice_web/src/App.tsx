c5f3c4bf8ec7b5dfcaf99ecd6b416a4ac878ff83
Cogeta <bongumusasizwemavuso@gmail.com>
2025-04-17 19:10:40 +0200

    init frontend

---
+++ frontend/univice_web/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1>Univice</h1>
+
+      <p className="read-the-docs">
+        Discover. Decide. Do It.
+      </p>
+    </>
+  )
+}
+
+export default App
